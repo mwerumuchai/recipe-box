5ed2837f2186bae33e6817fccbb8e608651d7321
mwerumuchai <mwerumuchai@gmail.com>
2017-09-12 11:29:43 +0300

    Show recipes

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,31 +4,33 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
   <div class="container">
-    <h1><u> Recipes</u></h1>
-    <h3 (click)="doStuff(currentTask)" *ngFor="let currentTask of tasks">{{ currentTask.description }}</h3>
+    <h1>Recipe Box</h1>
+    <h3 (click)="doStuff(currentRecipe)">{{ type }}</h3>
+    <div class="well" *ngFor="let currentRecipe of recipes">
+      <h4>{{currentRecipe.title}}</h4>
+      <h5>{{currentRecipe.ingredients}}</h5>
+      <p>{{currentRecipe.directions}}</p>
+    </div>
   </div>
   `
 })
 
 export class AppComponent {
-  public tasks: Task[] = [
-      new Task("Rice and Chicken Recipe.", 0),
-      new Task("Rice and Beef Recipe.", 1),
-      new Task("Pasta Recipe.", 2),
-    ];
-  doStuff(clickedTask: Task) {
-    if(clickedTask.done === true) {
-      alert("This task is done!");
-    } else {
-      alert("Ingredients and Instructions:");
-    }
-  }
+  type: string = '';
+  recipes: Recipe[] = [
+    new Recipe('Chicken', 'Chicken thighs,cilantro sauce and vegetable oil', 'Mix all the ingrdients and Roast the chicken for 25 minutes, 180 degrees '),
+    new Recipe('Beef', 'Meat, Onions, vegetable oil', 'Add zucchini, tomatoes, onions and capsicum. After a few minutes add beef and cook for 30 minutes'),
+    new Recipe('Pasta', 'Water and Pasta', 'Bring a large pot of water to a boil. Add pasta and cook according to package direction')
+  ];
+/*  doStuff(clickTask: Recipe) {
+    alert
+  }*/
 }
 
-export class Task {
-  public done: boolean = false;
-  constructor(public description: string, public id: number) {   }
+export class Recipe {
+  constructor(public title: string, public ingredients: string, public directions: string) {}
 }
+
 
 /*{{ }}:moustache tags: they cause any expression/variables inside of the double curly brackets to be evaluatedso that their value is displayed
  task 1: boolean value named done and its default is false
