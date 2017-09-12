b7fbfb6c8099542e0c3c1e91e76291d2823614d8
mwerumuchai <mwerumuchai@gmail.com>
2017-09-12 10:23:24 +0300

    Include recipes

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
   <div class="container">
-    <h1>My First Angular 2 App</h1>
+    <h1><u> Recipes</u></h1>
     <h3 (click)="doStuff(currentTask)" *ngFor="let currentTask of tasks">{{ currentTask.description }}</h3>
   </div>
   `
@@ -12,16 +12,15 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   public tasks: Task[] = [
-      new Task("Create To-Do List app.", 0),
-      new Task("Learn Kung Fu.", 1),
-      new Task("Rewatch all the Lord of the Rings movies.", 2),
-      new Task("Do the laundry.", 3)
-  ];
+      new Task("Rice and Chicken Recipe.", 0),
+      new Task("Rice and Beef Recipe.", 1),
+      new Task("Pasta Recipe.", 2),
+    ];
   doStuff(clickedTask: Task) {
     if(clickedTask.done === true) {
       alert("This task is done!");
     } else {
-      alert("This task is not completed yet! Get to work!");
+      alert("Ingredients and Instructions:");
     }
   }
 }
@@ -33,5 +32,6 @@ export class Task {
 
 /*{{ }}:moustache tags: they cause any expression/variables inside of the double curly brackets to be evaluatedso that their value is displayed
  task 1: boolean value named done and its default is false
- task 2: description TASK 3: an Id to assign each task a unique number
+ task 2: description
+ TASK 3: an Id to assign each task a unique number
  use an output bunding when you want to trigger an event from a DOM element*/
