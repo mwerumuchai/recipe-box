c6253f8289bd58abddd323445374542f3fc6d614
mwerumuchai <mwerumuchai@gmail.com>
2017-09-13 07:47:37 +0300

    display recipes

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
     <h1>Recipe Box</h1>
-    <h3 (click)="doStuff(currentRecipe)">{{ type }}</h3>
+    <h3>{{ type }}</h3>
     <div class="well" *ngFor="let currentRecipe of recipes">
       <h4>{{currentRecipe.title}}</h4>
       <h5>{{currentRecipe.ingredients}}</h5>
@@ -16,7 +16,7 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  type: string = '';
+  type: string = 'Meals';
   recipes: Recipe[] = [
     new Recipe('Chicken', 'Chicken thighs,cilantro sauce and vegetable oil', 'Mix all the ingrdients and Roast the chicken for 25 minutes, 180 degrees '),
     new Recipe('Beef', 'Meat, Onions, vegetable oil', 'Add zucchini, tomatoes, onions and capsicum. After a few minutes add beef and cook for 30 minutes'),
@@ -36,4 +36,6 @@ export class Recipe {
  task 1: boolean value named done and its default is false
  task 2: description
  TASK 3: an Id to assign each task a unique number
- use an output bunding when you want to trigger an event from a DOM element*/
+ use an output bunding when you want to trigger an event from a DOM elemen
+title,ingredientsand directions are properties.
+ t*/
